afecfbbab5cee82317cadbbf2d384733f3bd25e3
Петренко Игорь Вадимович <igor333.petrenko@yande.ru>
2025-11-10 16:17:51 +0300

    Добавит настройки миграций и добавит импорт

--- src/main/java/com/example/restfullservice/repository/UserRepository.java
+++ src/main/java/com/example/restfullservice/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.restfullservice.repository;
 
+import com.example.restfullservice.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
