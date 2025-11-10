f8b50c8b8abc17dfa166c68d1b22b4954201f16f
Петренко Игорь Вадимович <igor333.petrenko@yande.ru>
2025-11-10 12:13:53 +0300

    Удалит избыточный запрос

--- src/main/java/com/example/restfullservice/repository/UserRepository.java
+++ src/main/java/com/example/restfullservice/repository/UserRepository.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(value = "select * from users where email = :email", nativeQuery = true)
     Optional<User> findByEmail(String email);
 
     Optional<User> findByName(String name);
